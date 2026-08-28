9d2f8c3b362557b1cdbdc5809b67c51a2c5c2997
Brad Traversy <traversymedia@gmail.com>
2026-08-28 12:28:36 -0400

    multiprompter 1.3

--- Casks/multiprompter.rb
+++ Casks/multiprompter.rb
@@ -1,6 +1,6 @@
 cask "multiprompter" do
-  version "1.2"
-  sha256 "f5216562466434e87b92f490235a1c7c552c6a20851795df62f76bb883550e3a"
+  version "1.3"
+  sha256 "1037950bd14c54cc0952e516d084376b0fa4514ef1dfd09b6770e8448c3811f0"
 
   url "https://github.com/multiprompter/releases/releases/download/v#{version}/Multiprompter.dmg"
   name "Multiprompter"
